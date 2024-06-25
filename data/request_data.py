ff8c3c6d41eded773b22f934e13dd0d13c678ecf
Henriquerezer <hrezermosquer75@gmail.com>
2024-06-24 22:36:12 -0300

    results

--- data/request_data.py
+++ data/request_data.py
@@ -175,82 +175,216 @@ print("Dados salvos no banco de dados com sucesso.")
 
 # %%
 # DADOS DE CADA CORRIDA
+
+import requests
+import xml.etree.ElementTree as ET
+import pandas as pd
+from tqdm import tqdm
+from sqlalchemy import create_engine
+import sqlalchemy
+
+def extract_race_data(year, round_number):
+    url = f"http://ergast.com/api/f1/{year}/{round_number}/results"
+    response = requests.get(url)
+    response_text = response.text
+    root = ET.fromstring(response_text)
+
+    namespace = {'ns': 'http://ergast.com/mrd/1.5'}
+    data = []
+
+    race_table = root.find('.//ns:RaceTable', namespace)
+    season = race_table.attrib.get('season')
+    round_number = race_table.attrib.get('round')
+    race = race_table.find('.//ns:Race', namespace)
+    circuit = race.find('ns:Circuit', namespace)
+    circuit_id = circuit.attrib.get('circuitId')
+
+    for result in root.findall('.//ns:Result', namespace):
+        position = result.attrib.get('position')
+        points = result.attrib.get('points')
+        
+        driver = result.find('ns:Driver', namespace)
+        driver_id = driver.attrib.get('driverId')
+        code = driver.attrib.get('code')
+        permanent_number = driver.find('ns:PermanentNumber', namespace).text
+        given_name = driver.find('ns:GivenName', namespace).text
+        family_name = driver.find('ns:FamilyName', namespace).text
+        date_of_birth = driver.find('ns:DateOfBirth', namespace).text
+        nationality = driver.find('ns:Nationality', namespace).text
+        
+        constructor = result.find('ns:Constructor', namespace)
+        constructor_name = constructor.find('ns:Name', namespace).text
+        constructor_nationality = constructor.find('ns:Nationality', namespace).text
+        
+        grid = result.find('ns:Grid', namespace).text
+        laps = result.find('ns:Laps', namespace).text
+        status = result.find('ns:Status', namespace).text
+        time_element = result.find('ns:Time', namespace)
+        time = time_element.text if time_element is not None else 0
+        fastest_lap = result.find('ns:FastestLap/ns:Time', namespace)
+        fastest_lap_time = fastest_lap.text if fastest_lap is not None else 0
+        
+        data.append({
+            'Season': season,
+            'Round': round_number,
+            'CircuitID': circuit_id,
+            'Position': position,
+            'Points': points,
+            'DriverID': driver_id,
+            'Code': code,
+            'PermanentNumber': permanent_number,
+            'GivenName': given_name,
+            'FamilyName': family_name,
+            'DateOfBirth': date_of_birth,
+            'Nationality': nationality,
+            'ConstructorName': constructor_name,
+            'ConstructorNationality': constructor_nationality,
+            'Grid': grid,
+            'Laps': laps,
+            'Status': status,
+            'Time': time,
+            'FastestLapTime': fastest_lap_time
+        })
+    
+    return data
+
+def get_total_rounds(year):
+    url = f"http://ergast.com/api/f1/{year}.xml"
+    response = requests.get(url)
+    response_text = response.text
+    root = ET.fromstring(response_text)
+
+    namespace = {'ns': 'http://ergast.com/mrd/1.5'}
+    total_rounds = len(root.findall('.//ns:Race', namespace))
+    return total_rounds
+
+years = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]
+all_race_data = []
+
+# Usar tqdm para exibir uma barra de progresso
+for year in tqdm(years, desc="Fetching race data by year"):
+    total_rounds = get_total_rounds(year)
+    for round_number in tqdm(range(1, total_rounds + 1), desc=f"Year {year}", leave=False):
+        race_data = extract_race_data(year, round_number)
+        all_race_data.extend(race_data)
+
+# Criar o dataframe com todos os dados
+df = pd.DataFrame(all_race_data)
+
+engine = create_engine('sqlite:///../data/F1_DATA.db')
+# Salva o DataFrame em uma tabela chamada "drivers" no banco de dados SQLite
+df.to_sql('results', con=engine, if_exists='replace', index=False)
+print("Dados salvos no banco de dados com sucesso.")
+
+# %%
+# DADOS DE NOVAS CORRIDAS
+
 import requests
 import xml.etree.ElementTree as ET
 import pandas as pd
 from tqdm import tqdm
 
-def fetch_results(year, num_rounds):
-    all_results = []
+def extract_race_data(year, round_number):
+    url = f"http://ergast.com/api/f1/{year}/{round_number}/results"
+    response = requests.get(url)
+    response_text = response.text
+    root = ET.fromstring(response_text)
 
-    for round_num in tqdm(range(1, num_rounds+1), desc=f"Progresso - {year}"):
-        url = f"http://ergast.com/api/f1/{year}/{round_num}/results"
-        response = requests.get(url)
+    namespace = {'ns': 'http://ergast.com/mrd/1.5'}
+    data = []
 
-        if response.status_code != 200:
-            print(f"Erro ao obter os dados para {year}/{round_num}: {response.status_code}")
-            continue
+    race_table = root.find('.//ns:RaceTable', namespace)
+    if race_table is None:
+        return data  # Se não houver RaceTable, retornar lista vazia
+    
+    season = race_table.attrib.get('season')
+    round_number = race_table.attrib.get('round')
+    race = race_table.find('.//ns:Race', namespace)
+    if race is None:
+        return data  # Se não houver Race, retornar lista vazia
+    
+    circuit = race.find('ns:Circuit', namespace)
+    if circuit is None:
+        return data  # Se não houver Circuit, retornar lista vazia
+    
+    circuit_id = circuit.attrib.get('circuitId')
 
-        root = ET.fromstring(response.text)
-        race_table = root.find("{http://ergast.com/mrd/1.4}RaceTable")
-        if race_table is None:
-            print(f"Sem dados para {year}/{round_num}")
-            continue  # Não há dados para essa corrida
+    for result in root.findall('.//ns:Result', namespace):
+        position = result.attrib.get('position')
+        points = result.attrib.get('points')
+        
+        driver = result.find('ns:Driver', namespace)
+        driver_id = driver.attrib.get('driverId')
+        code = driver.attrib.get('code')
+        permanent_number = driver.find('ns:PermanentNumber', namespace).text if driver.find('ns:PermanentNumber', namespace) is not None else 0
+        given_name = driver.find('ns:GivenName', namespace).text if driver.find('ns:GivenName', namespace) is not None else 0
+        family_name = driver.find('ns:FamilyName', namespace).text if driver.find('ns:FamilyName', namespace) is not None else 0
+        date_of_birth = driver.find('ns:DateOfBirth', namespace).text if driver.find('ns:DateOfBirth', namespace) is not None else 0
+        nationality = driver.find('ns:Nationality', namespace).text if driver.find('ns:Nationality', namespace) is not None else 0
+        
+        constructor = result.find('ns:Constructor', namespace)
+        constructor_name = constructor.find('ns:Name', namespace).text if constructor.find('ns:Name', namespace) is not None else 0
+        constructor_nationality = constructor.find('ns:Nationality', namespace).text if constructor.find('ns:Nationality', namespace) is not None else 0
+        
+        grid = result.find('ns:Grid', namespace).text if result.find('ns:Grid', namespace) is not None else 0
+        laps = result.find('ns:Laps', namespace).text if result.find('ns:Laps', namespace) is not None else 0
+        status = result.find('ns:Status', namespace).text if result.find('ns:Status', namespace) is not None else 0
+        time_element = result.find('ns:Time', namespace)
+        time = time_element.text if time_element is not None else 0
+        fastest_lap = result.find('ns:FastestLap/ns:Time', namespace)
+        fastest_lap_time = fastest_lap.text if fastest_lap is not None else 0
+        
+        data.append({
+            'Season': season,
+            'Round': round_number,
+            'CircuitID': circuit_id,
+            'Position': position,
+            'Points': points,
+            'DriverID': driver_id,
+            'Code': code,
+            'PermanentNumber': permanent_number,
+            'GivenName': given_name,
+            'FamilyName': family_name,
+            'DateOfBirth': date_of_birth,
+            'Nationality': nationality,
+            'ConstructorName': constructor_name,
+            'ConstructorNationality': constructor_nationality,
+            'Grid': grid,
+            'Laps': laps,
+            'Status': status,
+            'Time': time,
+            'FastestLapTime': fastest_lap_time
+        })
+    
+    return data
 
-        for result in race_table.findall(".//{http://ergast.com/mrd/1.4}Result"):
-            result_data = {
-                "year": year,
-                "round": round_num,
-                "number": result.get("number"),
-                "position": result.get("position"),
-                "points": result.get("points"),
-                "grid": result.find("{http://ergast.com/mrd/1.4}Grid").text,
-                "laps": result.find("{http://ergast.com/mrd/1.4}Laps").text,
-                "status": result.find("{http://ergast.com/mrd/1.4}Status").text
-            }
-            driver = result.find("{http://ergast.com/mrd/1.4}Driver")
-            result_data["driverId"] = driver.get("driverId")
-            result_data["givenName"] = driver.find("{http://ergast.com/mrd/1.4}GivenName").text
-            result_data["familyName"] = driver.find("{http://ergast.com/mrd/1.4}FamilyName").text
-            result_data["dateOfBirth"] = driver.find("{http://ergast.com/mrd/1.4}DateOfBirth").text
-            result_data["nationality"] = driver.find("{http://ergast.com/mrd/1.4}Nationality").text
-            constructor = result.find("{http://ergast.com/mrd/1.4}Constructor")
-            result_data["constructorId"] = constructor.get("constructorId")
-            result_data["constructorName"] = constructor.find("{http://ergast.com/mrd/1.4}Name").text
-            result_data["constructorNationality"] = constructor.find("{http://ergast.com/mrd/1.4}Nationality").text
-            all_results.append(result_data)
+def get_total_rounds(year):
+    url = f"http://ergast.com/api/f1/{year}.xml"
+    response = requests.get(url)
+    response_text = response.text
+    root = ET.fromstring(response_text)
 
-    return all_results
+    namespace = {'ns': 'http://ergast.com/mrd/1.5'}
+    total_rounds = len(root.findall('.//ns:Race', namespace))
+    return total_rounds
 
-# Anos para os quais você deseja buscar os resultados
-anos = list(range(2023, 2024))
-num_rounds = 22  # Assumindo que há 22 corridas em cada temporada
+years = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]
+all_race_data = []
 
-# Inicializa a lista de todos os resultados
-all_results = []
+# Usar tqdm para exibir uma barra de progresso
+for year in tqdm(years, desc="Fetching race data by year"):
+    total_rounds = get_total_rounds(year)
+    for round_number in tqdm(range(1, total_rounds + 1), desc=f"Year {year}", leave=False):
+        race_data = extract_race_data(year, round_number)
+        all_race_data.extend(race_data)
 
-# Itera sobre os anos e busca os resultados para cada ano
-for year in anos:
-    results = fetch_results(year, num_rounds)
-    all_results.extend(results)
+# Criar o dataframe com todos os dados
+df = pd.DataFrame(all_race_data)
 
-# Cria o DataFrame com todos os dados de resultados
-df = pd.DataFrame(all_results)
-print(df)
+# Mostrar o dataframe
+df
 
-
-# %%
-
-import requests
-
-url = "http://ergast.com/api/f1/2023/20/results"
-
-payload={}
-headers = {}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
-
-
-
+engine = create_engine('sqlite:///../data/F1_DATA.db')
+# Salva o DataFrame em uma tabela chamada "drivers" no banco de dados SQLite
+df.to_sql('circuits', con=engine, if_exists='replace', index=False)
+print("Dados salvos no banco de dados com sucesso.")
